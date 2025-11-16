4e68212a2e8b8c29d5214cf681cd70d3c56fd768
ankushKun <ankush4singh@gmail.com>
2025-11-16 14:47:54 +0530

    build fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  reactStrictMode: false,
+  transpilePackages: ['@rainbow-me/rainbowkit'],
+  serverExternalPackages: [
+    'pino',
+    'pino-pretty',
+    'thread-stream',
+    'lokijs',
+    'encoding',
+  ],
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
